e75b0c0220b51a0f0f807ee42176c73c4187ca83
Cristian Sepúlveda <csepulvedac@larrainvial.com>
2024-09-17 10:49:00 -0300

    ADD finish switch-theme

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -2,11 +2,19 @@
 'use client'
 
 import {NextUIProvider} from '@nextui-org/react'
+import {ThemeProvider as NextThemesProvider} from "next-themes";
+import { UserProvider } from '@auth0/nextjs-auth0/client';
+
 
 export function Providers({ children }) {
   return (
-    <NextUIProvider>
-      {children}
-    </NextUIProvider>
+    <UserProvider>
+      <NextUIProvider>
+        <NextThemesProvider attribute="class" defaultTheme="dark">
+          {children}
+        </NextThemesProvider>
+      </NextUIProvider>
+    </UserProvider>
+    
   )
 }
